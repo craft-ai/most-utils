f07c6c5baaf1a4e73e8e468f6b17fe21e2f5679d
Clodéric Mars <cloderic.mars@gmail.com>
2017-08-07 18:47:02 +0200

    Fix the export definition for the limiter

--- packages/most-limiter/src/most-limiter.d.ts
+++ packages/most-limiter/src/most-limiter.d.ts
@@ -1,4 +1,3 @@
 import { Stream } from 'most';
 
-export function limiter<T>(interval: number, capacity?: number): (stream: Stream<T>) => Stream<T>;
-export default limiter;
+export default function limiter<T>(interval: number, capacity?: number): (stream: Stream<T>) => Stream<T>;
