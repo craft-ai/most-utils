3116e40eef45853e9dd44c01b52d601c38bddcea
Clodéric Mars <cloderic.mars@craft.ai>
2017-08-08 11:19:58 +0200

    Using a `reduce` in the tests

--- packages/most-buffer/test/test-buffer.js
+++ packages/most-buffer/test/test-buffer.js
@@ -4,33 +4,27 @@ const most = require('most');
 
 describe('buffer', function() {
   it('can group stream events 10 by 10', function() {
-    let count = 0;
     return most.iterate(x => x + 1, 0)
       .take(25)
       .thru(buffer(10))
-      .tap(() => { count += 1; })
       .tap(buffer => expect(buffer).to.have.lengthOf.at.most(10))
-      .drain()
-      .then(() => expect(count).to.be.equal(3));
+      .reduce(count => count + 1, 0)
+      .then(count => expect(count).to.be.equal(3));
   });
   it('can group stream events 50 by 50', function() {
-    let count = 0;
     return most.iterate(x => x + 1, 0)
       .take(25)
       .thru(buffer(50))
-      .tap(() => { count += 1; })
       .tap(buffer => expect(buffer).to.have.lengthOf(25))
-      .drain()
-      .then(() => expect(count).to.be.equal(1));
+      .reduce(count => count + 1, 0)
+      .then(count => expect(count).to.be.equal(1));
   });
   it('wait for the source stream to be finished emitting anything when no count is given', function() {
-    let count = 0;
     return most.iterate(x => x + 1, 0)
       .take(25)
       .thru(buffer())
-      .tap(() => { count += 1; })
       .tap(buffer => expect(buffer).to.have.lengthOf(25))
-      .drain()
-      .then(() => expect(count).to.be.equal(1));
+      .reduce(count => count + 1, 0)
+      .then(count => expect(count).to.be.equal(1));
   });
 });
