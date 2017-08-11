b2bb2adf3f8f36d16bf8564cb0384cfb7d1dc4ab
Clodéric Mars <cloderic.mars@craft.ai>
2017-08-11 17:04:08 +0200

    Some comments in the implementation

--- packages/most-nth/src/most-nth.js
+++ packages/most-nth/src/most-nth.js
@@ -2,12 +2,15 @@ function nth(desiredIndex) {
   if (desiredIndex < 0) {
     return (stream) => stream
       .reduce((lastEvents, event) => {
+        // Keeping a buffer of the nth last received events to be able to retrieve the desired one.
         lastEvents.push(event);
         if (lastEvents.length > -desiredIndex) {
+          // Buffer is full, Poping the earliest event from the buffer
           return lastEvents.slice(1);
         }
         return lastEvents;
       }, [])
+      // Checking if we actually reached the desired index
       .then((lastEvents) => lastEvents.length === -desiredIndex ? lastEvents[0] : undefined);
   }
   else {
@@ -17,6 +20,7 @@ function nth(desiredIndex) {
         index: index + 1,
         event: currentEvent
       }), { index: -1 })
+      // Checking if we actually reached the desired index
       .then(({ event, index }) => index === desiredIndex ? event : undefined);
   }
 }
