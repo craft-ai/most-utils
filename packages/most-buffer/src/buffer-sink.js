d121cfa0e06dc72b86894ea803377c31942ab012
alexisfontaine <alexis.fontaine@outlook.com>
2018-03-31 02:47:16 +0200

    Skip remaining events after the end of a stream

--- packages/most-buffer/src/buffer-sink.js
+++ packages/most-buffer/src/buffer-sink.js
@@ -1,11 +1,16 @@
 class BufferSink {
   constructor(count, sink) {
+    this.active = true;
     this.sink = sink;
     this.count = count;
     this.buffer = [];
   }
 
   event(time, value) {
+    if (!this.active) {
+      return;
+    }
+
     // Buffering the new value
     this.buffer.push(value);
 
@@ -22,11 +27,16 @@ class BufferSink {
   }
 
   end(time, value) {
+    if (!this.active) {
+      return;
+    }
+
     // Sending what's left in the buffer
     this.sink.event(time, this.buffer);
     this.buffer = [];
 
     // And ending everything
+    this.active = false;
     this.sink.end(time, value);
   }
 }
