6ca33a347d6c8070a68d565b69b08049bf29989f
alexisfontaine <alexis.fontaine@outlook.com>
2018-03-31 02:32:07 +0200

    Add a test on skipping remaining events after end of stream
    From a test of `SafeSink` in Most.js repository: https://github.com/cujojs/most/blob/master/test/sink/SafeSink-test.js#L67-L78

--- packages/most-buffer/test/test-buffer.js
+++ packages/most-buffer/test/test-buffer.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
 const buffer = require('../src');
+const BufferSink = require('../src/buffer-sink');
 const most = require('most');
 
 describe('buffer', function() {
@@ -26,5 +27,25 @@ describe('buffer', function() {
       .tap((buffer) => expect(buffer).to.have.lengthOf(25))
       .reduce((count) => count + 1, 0)
       .then((count) => expect(count).to.be.equal(1));
+  });
+  it('skip remaining events after the end of a stream', function() {
+    const time = 123;
+    const expected = {};
+
+    const sink = new BufferSink(undefined, {
+      event(t, x) {
+        expect(t).to.equal(time);
+        expect(x).to.be.an.instanceof(Array).and.to.have.lengthOf(0);
+      },
+      error() { throw new Error('Should not be called'); },
+      end(t, x) {
+        expect(t).to.equal(time);
+        expect(x).to.equal(expected);
+      }
+    });
+
+    sink.end(time, expected);
+    sink.end(time + 1);
+    sink.event(time + 2);
   });
 });
