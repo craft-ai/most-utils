f58a6b6922fb830f0883456f26eb1a3c93de926e
alexisfontaine <alexis.fontaine@outlook.com>
2018-03-31 02:46:02 +0200

    Assert that buffer is an array before checking its length

--- packages/most-buffer/test/test-buffer.js
+++ packages/most-buffer/test/test-buffer.js
@@ -8,25 +8,25 @@ describe('buffer', function() {
     return most.iterate((x) => x + 1, 0)
       .take(25)
       .thru(buffer(10))
-      .tap((buffer) => expect(buffer).to.have.lengthOf.at.most(10))
+      .tap((buffer) => expect(buffer).to.be.an.instanceof(Array).and.to.have.lengthOf.at.most(10))
       .reduce((count) => count + 1, 0)
-      .then((count) => expect(count).to.be.equal(3));
+      .then((count) => expect(count).to.equal(3));
   });
   it('can group stream events 50 by 50', function() {
     return most.iterate((x) => x + 1, 0)
       .take(25)
       .thru(buffer(50))
-      .tap((buffer) => expect(buffer).to.have.lengthOf(25))
+      .tap((buffer) => expect(buffer).to.be.an.instanceof(Array).and.to.have.lengthOf(25))
       .reduce((count) => count + 1, 0)
-      .then((count) => expect(count).to.be.equal(1));
+      .then((count) => expect(count).to.equal(1));
   });
   it('wait for the source stream to be finished emitting anything when no count is given', function() {
     return most.iterate((x) => x + 1, 0)
       .take(25)
       .thru(buffer())
-      .tap((buffer) => expect(buffer).to.have.lengthOf(25))
+      .tap((buffer) => expect(buffer).to.be.an.instanceof(Array).and.to.have.lengthOf(25))
       .reduce((count) => count + 1, 0)
-      .then((count) => expect(count).to.be.equal(1));
+      .then((count) => expect(count).to.equal(1));
   });
   it('skip remaining events after the end of a stream', function() {
     const time = 123;
