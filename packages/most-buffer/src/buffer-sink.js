c8fc9a13e3afc7b9fa3206c252f1f06320056298
ppoliani <ppoliani@gmail.com>
2018-05-14 17:47:38 +0100

    Add an optional flush period
    Sometimes the sink might not accumulate the buffer size for a long time. In cases like that, it would be nice to flush the buffer withtin a specific time period
    and push the values to the subscribers

--- packages/most-buffer/src/buffer-sink.js
+++ packages/most-buffer/src/buffer-sink.js
@@ -1,7 +1,8 @@
 class BufferSink {
-  constructor(count, sink) {
+  constructor(count, sink, flushAfter) {
     this.active = true;
     this.sink = sink;
+    this.flushAfter = flushAfter;
     this.count = count;
     this.buffer = count === undefined ? [] : new Array(count);
     this.length = 0;
@@ -14,6 +15,19 @@ class BufferSink {
 
     // Buffering the new value
     this.buffer[this.length++] = value;
+
+    if(this.flushAfter) {
+      // make sure we flush this if some time has passed and count is not reached
+      this.flushTimeout && clearTimeout(this.flushTimeout);
+
+      setTimeout(() => {
+        if(this.length > 0) {
+          console.log('setTimeout')
+          this.sink.event(time, value);
+          this.length = 0;
+        }
+      }, this.flushAfter)
+    }
 
     // If the buffer has a limit and is full, let's emit it
     if (this.length === this.count) {
